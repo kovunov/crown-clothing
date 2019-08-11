b9b4b0bcf0cd354bcee89a8a4927226003fb56ae
Anton Kovunov <antonkovunov@Antons-MacBook-Pro.local>
2019-08-11 15:59:29 -0400

    Initial commit

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './homepage.scss';
+
+export default function HomePage() {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <h1 className='subtitle'>SHOP NOW</h1>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>JACKETS</h1>
+                        <h1 className='subtitle'>SHOP NOW</h1>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SNEAKERS</h1>
+                        <h1 className='subtitle'>SHOP NOW</h1>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MEN</h1>
+                        <h1 className='subtitle'>SHOP NOW</h1>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>WOMEN</h1>
+                        <h1 className='subtitle'>SHOP NOW</h1>
+                    </div>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
